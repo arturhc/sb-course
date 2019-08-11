f74367b1a71e89b2af2653f8f9a51647e084ddfa
vakmancordero <vakmancordero@gmail.com>
2019-08-11 11:34:47 -0500

    adding datarunner

--- users/src/main/java/com/kaizensoftware/users/runner/DataRunner.java
+++ users/src/main/java/com/kaizensoftware/users/runner/DataRunner.java
@@ -1,25 +1,29 @@
 package com.kaizensoftware.users.runner;
 
-import com.kaizensoftware.users.persistence.model.*;
-import com.kaizensoftware.users.persistence.repository.*;
+import com.kaizensoftware.users.persistence.model.Authority;
+import com.kaizensoftware.users.persistence.model.Role;
+import com.kaizensoftware.users.persistence.model.RoleAuthority;
+import com.kaizensoftware.users.persistence.model.User;
+import com.kaizensoftware.users.persistence.repository.RoleRepository;
+import com.kaizensoftware.users.persistence.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import lombok.Setter;
+import java.util.Optional;
 
 @Component
 public class DataRunner implements CommandLineRunner {
 
-    @Setter(onMethod = @__(@Autowired))
+    @Autowired
     private UserRepository userRepository;
 
-    @Setter(onMethod = @__(@Autowired))
+    @Autowired
     private RoleRepository roleRepository;
 
-    @Setter(onMethod = @__(@Autowired))
+    @Autowired
     private PasswordEncoder passwordEncoder;
 
     @Override
@@ -28,28 +32,36 @@ public class DataRunner implements CommandLineRunner {
         User user = new User();
         user.setEmail("arturh.sw@gmail.com");
 
-        if (!userRepository.findByEmail(user.getEmail()).isPresent()) {
+        Optional<User> optionalUser = userRepository.findByEmail(user.getEmail());
 
+        if (!optionalUser.isPresent()) {
 
             user.setFullName("Arturo Cordero Muñiz");
-            user.setPassword(passwordEncoder.encode("jaqart_"));
+            user.setPassword(passwordEncoder.encode("lenovo_1234"));
 
             Role role = new Role();
             role.setName("ADMIN");
-            role.setDescription("Admin Role");
+            role.setDescription("Admin role");
 
-            if (!roleRepository.findByName(role.getName()).isPresent()) {
+            Optional<Role> optionalRole = roleRepository.findByName(role.getName());
 
-                role.getRoleAuthorities().add(
-                        new RoleAuthority(role, new Authority("READ_USERS")));
+            if (!optionalRole.isPresent()) {
+
+                Authority authority = new Authority("READ_USERS");
+
+                RoleAuthority roleAuthority = new RoleAuthority(role, authority);
+
+                role.getRoleAuthorities().add(roleAuthority);
 
                 roleRepository.save(role);
+
+                user.setRole(role);
+
+            } else {
+                user.setRole(optionalRole.get());
             }
 
-            user.setRole(role);
-
             userRepository.save(user);
-
         }
 
     }
